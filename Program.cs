3f8752baab2165f9f0fd535c9f39956d755d3fc4
joaquinlechesi <joaquinlechesi@gmail.com>
2025-10-23 18:12:42 -0300

    Insertar registros en PresupuestosDetalle y listar PresupuestosDetalle funciona

--- Program.cs
+++ Program.cs
@@ -14,14 +14,23 @@ using (SqliteConnection connection = new SqliteConnection(connectionString))
     //     createTableCmd.ExecuteNonQuery();
     //     Console.WriteLine("Tabla 'productos' creada o ya existe.");
     // }
-    
+
     // Insertar datos
-    string insertQuery = "INSERT INTO Presupuestos (NombreDestinatario, FechaCreacion) VALUES ('Carlos Ruiz', '2014-10-25')";
+    //string insertQuery = "INSERT INTO Presupuestos (NombreDestinatario, FechaCreacion) VALUES ('Carlos Ruiz', '2014-10-25')";
+    //using (SqliteCommand insertCmd = new SqliteCommand(insertQuery, connection))
+    //{
+    //    insertCmd.ExecuteNonQuery();
+    //    Console.WriteLine("Datos insertados en la tabla 'Presupuestos'.");
+    //} //Funciona
+
+    // c) Insertar registros en PresupuestosDetalle
+    string insertQuery = "INSERT INTO PresupuestosDetalle (idPresupuesto, idProducto, Cantidad) VALUES (1, 3, 2)";
     using (SqliteCommand insertCmd = new SqliteCommand(insertQuery, connection))
     {
         insertCmd.ExecuteNonQuery();
-        Console.WriteLine("Datos insertados en la tabla 'Presupuestos'.");
+        Console.WriteLine("Datos insertados en la tabla 'PresupuestosDetalle'.");
     } //Funciona
+    
     // Borrar datos
     //string deleteQuery = "INSERT INTO Productos (Descripcion, Precio) VALUES ('Mouse Cableado Logitech', 5000.0)";
     //using (SqliteCommand insertCmd = new SqliteCommand(insertQuery, connection))
@@ -52,16 +61,27 @@ using (SqliteConnection connection = new SqliteConnection(connectionString))
             //    }
             //}
             // SELECT de la tabla Presupuestos
-            string selectQuery = "SELECT * FROM Presupuestos";
+            //string selectQuery = "SELECT * FROM Presupuestos";
+            //using (SqliteCommand selectCmd = new SqliteCommand(selectQuery, connection))
+            //using (SqliteDataReader reader = selectCmd.ExecuteReader())
+            //{
+            //    Console.WriteLine("Datos en la tabla 'Presupuestos':");
+            //    while (reader.Read())
+            //    {
+            //        Console.WriteLine($"ID: {reader["idPresupuestos"]}, Nombre de Destinatario: {reader["NombreDestinatario"]}, Precio: {reader["FechaCreacion"]}");
+            //    }
+            //}
+            // SELECT de la tabla PresupuestosDetalle
+            string selectQuery = "SELECT * FROM PresupuestosDetalle";
             using (SqliteCommand selectCmd = new SqliteCommand(selectQuery, connection))
             using (SqliteDataReader reader = selectCmd.ExecuteReader())
             {
-                Console.WriteLine("Datos en la tabla 'Presupuestos':");
+                Console.WriteLine("Datos en la tabla 'PresupuestosDetalle':");
                 while (reader.Read())
                 {
-                    Console.WriteLine($"ID: {reader["idPresupuestos"]}, Nombre de Destinatario: {reader["NombreDestinatario"]}, Precio: {reader["FechaCreacion"]}");
+                    Console.WriteLine($"idPresupuesto: {reader["idPresupuesto"]}, idProducto: {reader["idProducto"]}, Cantidad: {reader["Cantidad"]}");
                 }
-            }            
+            }
 
             connection.Close(); // funcionando
 }
